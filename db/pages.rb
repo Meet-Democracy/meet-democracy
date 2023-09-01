d7290d2d11b49ae4a20466fbce51e7f0e431a067
Karim Semmoud <karim.semmoud@netoum.com>
2023-09-01 12:51:36 +0700

    Do not load welcome page from db seed

--- db/pages.rb
+++ db/pages.rb
@@ -1,6 +1,3 @@
-load Rails.root.join("db", "pages", "welcome_level_three_verified.rb")
-load Rails.root.join("db", "pages", "welcome_level_two_verified.rb")
-load Rails.root.join("db", "pages", "welcome_not_verified.rb")
 load Rails.root.join("db", "pages", "accessibility.rb")
 load Rails.root.join("db", "pages", "conditions.rb")
 load Rails.root.join("db", "pages", "privacy.rb")
