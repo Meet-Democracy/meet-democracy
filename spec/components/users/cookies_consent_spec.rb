627b99531f52d737c14b4ce10f02e534ff30bbe1
Ramzi Hemadou <ramzihemadou@gmail.com>
2023-09-14 17:58:32 +0100

    Add Test System for Cookies Consent and component

---
+++ spec/components/users/cookies_consent_spec.rb
@@ -0,0 +1,76 @@
+require "rails_helper"
+
+describe Users::CookiesConsentComponent do
+
+  describe "After User Login" do
+    let(:user) { create(:user) }
+    let(:component) { Users::CookiesConsentComponent.new }
+    before do
+      request.env["devise.mapping"] = Devise.mappings[:user]
+    end
+
+    context "when cookies consent is not accepted" do
+      it "shows cookies consent form" do
+        request.cookies[:cookie_eu_consented] = "false"
+        user = create(:user)
+
+        sign_in(user)
+
+        expect(user.cookies_consent).to eq(false)
+
+        render_inline component
+
+        expect(page).to have_css ".cookies-eu"
+      end
+    end
+
+    context "when cookies consent is accepted" do
+      it "doesnt show cookies consent form" do
+        request.cookies[:cookie_eu_consented] = "true"
+        user = create(:user)
+
+        sign_in(user)
+
+        render_inline component
+
+        expect(page).not_to have_css ".cookies-eu"
+      end
+    end
+  end
+
+  describe "After User Logout" do
+    let(:user) { create(:user) }
+    let(:component) { Users::CookiesConsentComponent.new }
+    before do
+      request.env["devise.mapping"] = Devise.mappings[:user]
+    end
+
+    context "when cookies consent is not accepted" do
+      it "shows cookies consent form" do
+        request.cookies[:cookie_eu_consented] = "false"
+        user = create(:user)
+
+        sign_in(user)
+        logout
+
+        render_inline component
+
+        expect(page).to have_css ".cookies-eu"
+      end
+    end
+
+    context "when cookies consent is accepted" do
+      it "doesnt show cookies consent form" do
+        request.cookies[:cookie_eu_consented] = "true"
+        user = create(:user)
+
+        sign_in(user)
+        logout
+
+        render_inline component
+
+        expect(page).not_to have_css ".cookies-eu"
+      end
+    end
+  end
+end
---
+++ spec/system/cookies_consent_spec.rb
@@ -0,0 +1,27 @@
+require "rails_helper"
+
+describe "Cookies Consent" do
+
+  context "User not logged in" do
+
+    scenario "Show by default" do
+
+      visit root_path
+
+      expect(page).to have_css ".cookies-eu"
+    end
+
+    scenario "Hide when accepted" do
+
+      visit root_path
+
+      within(".cookies-eu") do
+        click_button "OK"
+      end
+
+      visit root_path
+
+      expect(page).not_to have_css ".cookies-eu"
+    end
+  end
+end
