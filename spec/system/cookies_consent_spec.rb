7c8692d9f92b724e1bbbc8593405791c00d47683
Karim Semmoud <karim.semmoud@netoum.com>
2023-09-16 00:58:13 +0200

    fix tests

--- spec/system/cookies_consent_spec.rb
+++ spec/system/cookies_consent_spec.rb
@@ -16,4 +16,49 @@ describe "Cookies Consent" do
       expect(page).not_to have_css ".cookies-eu"
     end
   end
+
+  context "User logged in" do
+    scenario "Show by default" do
+      user = create(:user)
+      login_as(user)
+      visit root_path
+      expect(page).to have_css ".cookies-eu"
+    end
+
+    scenario "Hide when accepted after login" do
+      user = create(:user)
+      login_as(user)
+      visit root_path
+      within(".cookies-eu") do
+        click_button "OK"
+      end
+      expect(page).not_to have_css ".cookies-eu"
+    end
+
+    scenario "Do not show again when loggin out if accepted while login" do
+      user = create(:user)
+      login_as user
+      visit root_path
+      within(".cookies-eu") do
+        click_button "OK"
+      end
+      expect(page).not_to have_css ".cookies-eu"
+      logout
+      expect(page).not_to have_css ".cookies-eu"
+    end
+  
+    scenario "Do not show again when accepted before login" do
+      visit root_path
+      within(".cookies-eu") do
+        click_button "OK"
+      end
+      expect(page).not_to have_css ".cookies-eu"
+      user = create(:user)
+      login_as user
+      visit root_path
+      expect(page).not_to have_css ".cookies-eu"
+      logout
+      expect(page).not_to have_css ".cookies-eu"
+    end
+  end
 end
