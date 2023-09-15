97935451adebb64e90471b42d93611d2f86f4265
Karim Semmoud <karim.semmoud@netoum.com>
2023-09-16 00:57:20 +0200

    Remove comment

--- app/assets/javascripts/cookies_consent.js
+++ app/assets/javascripts/cookies_consent.js
@@ -3,7 +3,6 @@
   App.CookiesConsent = {
     initialize: function() {
       $(document).on("cookies-eu-acknowledged", function() {
-        // The user has acknowledged cookies! If they're logged in, update their preference.
         var current_user_id = $("html").data("current-user-id");
         if (current_user_id) {
           $.ajax({
